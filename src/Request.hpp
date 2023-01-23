0ecf9b141529ae51692a0afd8d0930db40fd4445
zemmsoares <zemmsoares@gmail.com>
2023-01-23 19:03:51 +0000

    gitfilterrepo

---
+++ src/Request.hpp
@@ -0,0 +1,63 @@
+#include "json.hpp"
+#include <curl/curl.h>
+#include <fstream>
+#include <iostream>
+#include <ncurses.h>
+#include <string>
+
+using json = nlohmann::json;
+
+class Request {
+    public:
+    Request() {}
+
+    //https://stackoverflow.com/questions/9786150/save-curl-content-result-into-a-string-in-c
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+    {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    }
+    void getRequests(){
+    
+    CURL *curl;
+    CURLcode res;
+    json newsJson;
+    std::string readBuffer;
+
+    /*read config json for sources and api key*/
+    std::ifstream jsonFile("./data/config.json");
+    json configJson;
+    jsonFile >> configJson;
+    jsonFile.close();
+     /*grab api key and sources from /data/config.json*/
+    std::string api_key = configJson["api_key"];
+    std::string sources = configJson["sources"];
+    std::string endpoint =
+        "https://newsapi.org/v2/top-headlines?apiKey=" + api_key +
+        "&sources=" + sources;
+
+    curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "Polynews/2.0");
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            res = curl_easy_perform(curl);
+            if (res == CURLE_OK) {
+                //std::cout << readBuffer;
+                //std::cout << "STARTED INSIDE REQUEST\n";
+                try {
+                    newsJson = json::parse(readBuffer);
+                    std::ofstream jsonFile("./data/data.json");
+                    jsonFile << newsJson.dump(4);
+                    jsonFile.close();
+                } catch (json::parse_error& e) {
+                    std::cout << "Error parsing json: " << e.what() << std::endl;
+                }
+                //std::cout << "FINISHED INSIDE REQUEST\n";
+      } else {
+            std::cout << "CURL NOT OK\n";
+      }
+    }
+  }
+};
